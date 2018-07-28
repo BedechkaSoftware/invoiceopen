471152ec748dd127c7e3325e4499006a1ec5f074
Dimitar Yoldov <dimitar.yoldov@doitwise.com>
2018-07-28 03:11:48 +0300

    Fixes to build.bat

--- build.bat
+++ build.bat
@@ -8,16 +8,16 @@
 :: Compiling |
 :: ===========
 
-"E:/Qt/Tools/mingw530_32/bin/mingw32-make.exe" -f D:/jenkins/workspace/qt-test-application/application/Makefile.Debug
+"E:/Qt/Tools/mingw530_32/bin/mingw32-make.exe" -f D:/jenkins/workspace/qt-test-application/Makefile.Debug
 
 :: ======================================
 :: Moving .dll to the project directory |
 :: ======================================
 
-copy "E:\Qt\5.11.1\mingw53_32\bin\Qt5Cored.dll"       D:\jenkins\workspace\qt-test-application\application\debug\Qt5Cored.dll
-copy "E:\Qt\5.11.1\mingw53_32\bin\Qt5Widgetsd.dll"    D:\jenkins\workspace\qt-test-application\application\debug\Qt5Widgetsd.dll
-copy "E:\Qt\5.11.1\mingw53_32\bin\Qt5Guid.dll"        D:\jenkins\workspace\qt-test-application\application\debug\Qt5Guid.dll
-copy "E:\Qt\5.11.1\mingw53_32\bin\libgcc_s_dw2-1.dll" D:\jenkins\workspace\qt-test-application\application\debug\libgcc_s_dw2-1.dll
-copy "E:\Qt\5.11.1\mingw53_32\bin\libstdc++-6.dll"    "D:\jenkins\workspace\qt-test-application\application\debug\libstdc++-6.dll"
+copy "E:\Qt\5.11.1\mingw53_32\bin\Qt5Cored.dll"       D:\jenkins\workspace\qt-test-application\debug\Qt5Cored.dll
+copy "E:\Qt\5.11.1\mingw53_32\bin\Qt5Widgetsd.dll"    D:\jenkins\workspace\qt-test-application\debug\Qt5Widgetsd.dll
+copy "E:\Qt\5.11.1\mingw53_32\bin\Qt5Guid.dll"        D:\jenkins\workspace\qt-test-application\debug\Qt5Guid.dll
+copy "E:\Qt\5.11.1\mingw53_32\bin\libgcc_s_dw2-1.dll" D:\jenkins\workspace\qt-test-application\debug\libgcc_s_dw2-1.dll
+copy "E:\Qt\5.11.1\mingw53_32\bin\libstdc++-6.dll"    "D:\jenkins\workspace\qt-test-application\debug\libstdc++-6.dll"
 
-copy "E:\Qt\5.11.1\mingw53_32\bin\libwinpthread-1.dll"    "D:\jenkins\workspace\qt-test-application\application\debug\libwinpthread-1.dll"
+copy "E:\Qt\5.11.1\mingw53_32\bin\libwinpthread-1.dll"    "D:\jenkins\workspace\qt-test-application\debug\libwinpthread-1.dll"
